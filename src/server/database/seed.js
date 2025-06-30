973674d28c0ccc69d2c964a3af39773aa8e4cc65
Evan Hahn <me@evanhahn.com>
2025-06-30 21:01:31 +0000

    Database schema, seed, and basic functions

---
+++ src/server/database/seed.js
@@ -0,0 +1,33 @@
+import { addPerson, readPeople } from "./database.js";
+import * as fs from "node:fs";
+import * as path from "node:path";
+import { fileURLToPath } from "node:url";
+import { people } from "../hard-coded/people.js"; // TODO Consider moving this
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const hardCodedDirPath = path.join(__dirname, "..", "hard-coded");
+
+for (const person of people) {
+  const photoPath = path.join(hardCodedDirPath, person.person.photo);
+  addPerson({
+    name: person.person.name,
+    photo: fs.readFileSync(photoPath),
+    email: null,
+    values: person.values,
+    vision: person.vision,
+    vehicles: person.vehicles,
+  });
+}
+
+addPerson({
+  name: "Foo",
+  photo: null,
+  email: "foo@example.com",
+  locationName: "Colombia",
+  locationLatitude: 4.5709,
+  locationLongitude: -74.2973,
+  values: [],
+  vision: [],
+  vehicles: [],
+});
